173eb2356a7acc91d6ba011c6417693ea1739d71
CodeYoddha <imilanakumars@gmail.com>
2026-08-28 10:36:10 +0530

    Time: 5 ms (82.12%) | Memory: 45.9 MB (52.79%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,15 +1,22 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        //Negative Numbers are not Palindrome numbers
         if(x<0){
             return false;
         }
+        //Variable to store the Reversed Number
         int revNum = 0;
+        //Store the original number because x will be modified
         int dup = x;
         while(x>0){
+            //Get the last digit
             int lastDigit = x%10;
+            //Build the reversed number
             revNum = (revNum*10)+lastDigit;
+            //Remove the last Digit
             x = x/10;
         }
+        //Compare original and Reversed Number
         return dup==revNum;
     }
 }
